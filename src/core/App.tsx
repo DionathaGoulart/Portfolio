349cc97bdb712022d8a7eb8c8a9ed32fbc6f3ff8
goodgd <dionatha.work@gmail.com>
2025-08-14 16:58:59 -0300

    first commit

---
+++ src/core/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-5xl text-white">Tailwind Funcionando 🎉</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,scss}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

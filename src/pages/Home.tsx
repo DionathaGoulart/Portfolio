ba7bb6a14c30b7880cf77d7f0a15f0f5f064e1d8
goodgd <dionatha.work@gmail.com>
2025-08-17 23:11:20 -0300

    refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,8 @@
-// HomePage.tsx
 import React, { useEffect } from 'react'
 import { SectionConfig } from '@/shared'
 import HomeSection from './Sections/Home'
 import SobreSection from './Sections/Sobre'
 
-// 1. Definir a interface de props da página
 interface HomePageProps {
   setSections: (sections: SectionConfig[]) => void
   setPageTitle: (title: string) => void
@@ -18,19 +16,13 @@ export function HomePage({ setSections, setPageTitle }: HomePageProps) {
     { id: 'fale-comigo', label: 'Contato' }
   ]
 
-  // 2. Usa um useEffect para configurar o layout
   useEffect(() => {
-    // Envia a lista de seções para o RouteRenderer
     setSections(homePageSections)
-
-    // Define o título inicial da página
     setPageTitle('Dionatha')
-
-    // Função de limpeza: Opcional, mas útil para resetar ao sair da página
     return () => {
       setSections([])
     }
-  }, [setSections, setPageTitle]) // Dependências garantem que o efeito roda se as setters mudarem
+  }, [setSections, setPageTitle])
 
   return (
     <>
